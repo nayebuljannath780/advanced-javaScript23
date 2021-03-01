22ef8c01d5c462e65405a182f1149f06f0fafa67
nayebuljannath780 <chynayeb780@gmail.com>
2021-03-01 21:03:48 +0600

    complete

---
+++ null-vs-undefiend.js
@@ -0,0 +1,15 @@
+// let pakhi;
+
+// function add(x,y){
+//     console.log(x + y);
+//     return;
+// }
+
+
+// function add(num1, num2){
+//     console.log(num1, num2);
+// }
+// const result = add(10);
+
+const fun = { Name: "premik pusrush", phone: 1343432};
+console.log(fun.gf);
