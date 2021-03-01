6babe5634db4a6afb22a2b16c8142e05ad8576bb
nayebuljannath780 <chynayeb780@gmail.com>
2021-03-01 23:47:57 +0600

    sscpoe

---
+++ scope.js
@@ -0,0 +1,11 @@
+const bonus = 15;
+function add (num1, num2){
+    let result = num1 + num2 + bonus;
+    if(result > 4){
+        var mood = "happy";
+    }
+    console.log(mood);
+    return result;
+}
+const output = add(3, 7);
+console.log(output);
