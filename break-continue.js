e8cd353758333a9b8a4f19011f026ae8fde15e82
nayebuljannath780 <chynayeb780@gmail.com>
2021-03-02 03:05:54 +0600

    finish

--- break-continue.js
+++ break-continue.js
@@ -1,10 +1,10 @@
-// const num = [1,2,3,4,5,6,7,8];
-// for (let i = 0; i < num.length; i++) {
-//     if (num[i] > 3) {
-//         break;    
-//     }
-//     // console.log(num[i]);
-// }
+const num = [1,2,3,4,5,6,7,8];
+for (let i = 0; i < num.length; i++) {
+    if (num[i] > 3) {
+        break;    
+    }
+    // console.log(num[i]);
+}
 
 const num2 = [1, 2, -3, -4, 5, -6, 7, -8, 9];
 for (let i = 0; i < num2.length; i++) {
